ca773bee76dc1189133860c48a774a6046c756e8
fzzjj2008 <fzzjj2008@126.com>
2019-12-02 00:49:01 +0800

    add funcs

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-temp = input("please input number:")
-guess = int(temp)
+def fun(num):
+    print(num)
+
+def fun2():
+    pass
+
+# temp = input("please input number:")
+guess = 8
+# guess = int(temp)
+fun(guess)
 if guess == 8:
     print("guess ok")
 else:
     print("guess err")
 print("over")
+fun2()
